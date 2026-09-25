0fec4256efd2b972ee9b370e1386701daa637bad
Tanvir <tanvirahamed.foysal.00@gmail.com>
2026-09-26 02:01:19 +0600

    Update to deploy in firebase

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage(title: 'Flutter Demo Home Page Updated '),
+      home: const MyHomePage(title: 'Flutter Demo Home Page Updated 3'),
     );
   }
 }
